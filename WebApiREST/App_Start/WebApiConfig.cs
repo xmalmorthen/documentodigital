b440627413cb489afd286931c4f8de28b2197cbe
Xmal Morthen <xmal.morthen@gmail.com>
2013-09-23 00:06:13 -0500

    WebApiREST

---
+++ WebApiREST/App_Start/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace WebApiREST
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "Bucar en cualquier Expediente",
+                routeTemplate: "consulta/{controller}/{campo_trazable}",
+                defaults: null,
+                constraints: new { id_expediente = @"^[0-9]+$", campo_trazable = @"^[a-z]+$" }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "Bucar en un Expediente",
+                routeTemplate: "consulta/{controller}/{id_expediente}/{campo_trazable}",
+                defaults: null,
+                constraints: new { id_expediente = @"^[0-9]+$", campo_trazable = @"^[a-z]+$" }
+            );
+            
+        }
+    }
+}
---
+++ WebApiREST/Controllers/TramiteController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json;
+
+namespace WebApiREST.Controllers
+{
+    public class tramiteController : ApiController
+    {
+        // POST consulta/tramite/asd
+        public IEnumerable<string> Post(string campo_trazable)
+        {
+            return new string[] { campo_trazable, campo_trazable };
+        }
+
+        // POST consulta/tramite/1/asd
+        public IEnumerable<string> Post(int id_expediente, string campo_trazable)
+        {
+            return new string[] { id_expediente.ToString(), campo_trazable };
+        }
+       
+    }
+}
